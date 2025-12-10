7415d969d8dec4f189386248e77070178875d31d
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2025-12-10 16:46:34 -0500

    feat: lib

--- crates/burn-central-core/src/lib.rs
+++ crates/burn-central-core/src/lib.rs
@@ -1,3 +1,7 @@
+// Ensure at least one version is enabled
+#[cfg(not(any(feature = "burn_0_20", feature = "burn_0_19")))]
+compile_error!("At least one burn version feature must be enabled: burn_0_20 or burn_0_19");
+
 mod client;
 
 pub mod schemas;
@@ -21,7 +25,3 @@ pub use burn_0_20 as burn;
 
 #[cfg(feature = "burn_0_19")]
 pub use burn_0_19 as burn;
-
-// Ensure at least one version is enabled
-#[cfg(not(any(feature = "burn_0_20", feature = "burn_0_19")))]
-compile_error!("At least one burn version feature must be enabled: burn_0_20 or burn_0_19");
